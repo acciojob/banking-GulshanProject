e0e1a9a7e80ea169d4bc1160f42bd1952564e86a
GulshanProject <mandal.gulshan2003@gmail.com>
2022-12-31 14:03:31 +0530

    thirdAttempt

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -4,45 +4,33 @@ public class BankAccount {
 
     private String name;
     private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+
+    public double getMinBalance() {
+        return minBalance;
+    }
     private double minBalance;
 
-    public BankAccount(){
-
-    }
     public BankAccount(String name, double balance, double minBalance) {
               this.name=name;
               this.balance=balance;
               this.minBalance=minBalance;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public double getBalance() {
-        return balance;
-    }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
 
-    public double getMinBalance() {
-        return minBalance;
-    }
-
-    public void setMinBalance(double minBalance) {
-        this.minBalance = minBalance;
-    }
 
 
     public int DigitSum(int digit){
         int sumOfDigit=0;
-        while(digit>0){
+        while(digit!=0){
             int rem= digit%10;
             sumOfDigit+=rem;
             digit=digit/10;
@@ -51,7 +39,7 @@ public class BankAccount {
     }
     public String convertStringFromDigit(int digit){
         String AccountNo="";
-        while(digit>0){
+        while(digit!=0){
             int rem= digit%10;
             AccountNo+=rem;
             digit=digit/10;
